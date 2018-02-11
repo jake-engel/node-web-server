4d68f5572b500c5f0a08b22472ed8d5d1a249d3c
Jake Engel <jakeengel01@gmail.com>
2018-02-10 21:18:35 -0500

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -23,7 +24,7 @@ app.use((request, response, next) => {
 
 // app.use((req, res, next) => {
 //   res.render('maintenance.hbs');
-// }); // next() is not called. Thus maintenance will always be loaded if uncommented
+// }); // next() is not called. Thus maintenance will always be loaded if uncommentedh
 
 app.use(express.static(__dirname + '/public'));
 
@@ -55,7 +56,7 @@ app.get('/bad', (request, response) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
 
